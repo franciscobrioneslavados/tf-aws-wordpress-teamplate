6052b1291f37fdd2344f222dbeaa9f19c518ef41
Francisco Briones <franciscobrioneslavados@gmail.com>
2022-08-08 11:37:25 -0400

    update variables

--- variables.tf
+++ variables.tf
@@ -14,19 +14,21 @@ variable "secret_key" {
 variable "region" {
   type        = string
   description = "aws region"
-  default     = "us-east-1"
+  default     = ""
 }
 
 # Application definition
 variable "app_name" {
   type        = string
   description = "Application name"
+  default     = ""
 }
 
 # Application environment
 variable "app_environment" {
   type        = string
   description = "Application environment"
+  default     = ""
 }
 
 
@@ -34,16 +36,19 @@ variable "app_environment" {
 variable "db_name" {
   type        = string
   description = "value"
+  default     = ""
 }
 
 variable "db_username" {
   type        = string
   description = "value"
+  default     = ""
 }
 
 variable "db_password" {
   type        = string
   description = "value"
+  default     = ""
 }
 
 
